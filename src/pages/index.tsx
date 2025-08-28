dd27be5eb46684d4b1e0039dfd61c3ea64350c09
divyanshu0469 <divyanshuthegreat101@gmail.com>
2025-08-29 00:11:37 +0530

    fix: image parallax gap add and border radius add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -96,22 +96,26 @@ const RepeatedImages = ({
   repeat?: number;
   className?: string;
 }) => {
-  return Array(repeat)
-    .fill(0)
-    .map((_, idx) => (
-      <div key={idx} className="flex flex-col gap-4">
-        {images.map((src, index) => (
-          <Image
-            width={2268}
-            height={4032}
-            src={src}
-            key={index}
-            alt="parallax-image"
-            className={className}
-          />
+  return (
+    <div className="flex flex-col gap-4">
+      {Array(repeat)
+        .fill(0)
+        .map((_, idx) => (
+          <div key={idx} className="flex flex-col gap-4">
+            {images.map((src, index) => (
+              <Image
+                width={2268}
+                height={4032}
+                src={src}
+                key={index}
+                alt="parallax-image"
+                className={className}
+              />
+            ))}
+          </div>
         ))}
-      </div>
-    ));
+    </div>
+  );
 };
 
 const images = [
@@ -135,12 +139,12 @@ const ImageParallax = () => {
   });
   const { height } = dimension;
   const y1 = useTransform(scrollYProgress, [0, 1], [0, height * 1.75]);
-  const y2 = useTransform(scrollYProgress, [0, 1], [0, height * -0.89]);
+  const y2 = useTransform(scrollYProgress, [0, 1], [0, height * -0.2]);
   const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25]);
-  const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 0.89]);
+  const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 1]);
   useEffect(() => {
     const lenis = new Lenis();
-    const raf = (time) => {
+    const raf = (time: number) => {
       lenis.raf(time);
       requestAnimationFrame(raf);
     };
@@ -157,17 +161,33 @@ const ImageParallax = () => {
   return (
     <div className="w-full h-full overflow-hidden">
       <motion.div ref={ref} className="w-full h-full relative flex gap-4 px-4">
-        <motion.div style={{ y: y1 }}>
-          <RepeatedImages images={[images[0], images[3], images[6]]} />
+        <motion.div style={{ y: y1 }} className="-translate-y-full">
+          <RepeatedImages
+            images={[images[0], images[3], images[6]]}
+            className="rounded-md"
+            repeat={4}
+          />
         </motion.div>
         <motion.div style={{ y: y2 }}>
-          <RepeatedImages images={[images[4], images[7], images[1]]} />
+          <RepeatedImages
+            images={[images[4], images[7], images[1]]}
+            className="rounded-md"
+            repeat={4}
+          />
         </motion.div>
-        <motion.div style={{ y: y3 }}>
-          <RepeatedImages images={[images[5], images[2], images[8]]} />
+        <motion.div style={{ y: y3 }} className="-translate-y-full">
+          <RepeatedImages
+            images={[images[5], images[2], images[8]]}
+            className="rounded-md"
+            repeat={3}
+          />
         </motion.div>
-        <motion.div style={{ y: y4 }}>
-          <RepeatedImages images={[images[7], images[3], images[8]]} />
+        <motion.div style={{ y: y4 }} className="-translate-y-full">
+          <RepeatedImages
+            images={[images[7], images[3], images[8]]}
+            className="rounded-md"
+            repeat={4}
+          />
         </motion.div>
       </motion.div>
     </div>
