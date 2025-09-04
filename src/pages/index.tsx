7f1f81eb0e48cacb2c1f0f8c56445bc84741aafc
divyanshu0469 <divyanshuthegreat101@gmail.com>
2025-09-04 21:47:32 +0530

    feat: hide parallax if isMobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,20 @@ import { Heading } from "@/components/Heading";
 import { ImageParallax } from "@/components/ImageParallax";
 import { Stairs } from "@/components/Stairs";
 import { Work } from "@/components/Work";
+import { useMediaQuery } from "react-responsive";
 
 export default function About() {
+  const isMobile = useMediaQuery({ maxWidth: 637 });
   return (
     <Stairs>
       <div className="font-lt leading-[1] h-screen gap-3 flex flex-col justify-center items-center">
         <Heading />
       </div>
-      <div className="bg-light-cream text-light-black h-screen max-md:h-100 text-8xl font-lt">
-        <ImageParallax />
-      </div>
+      {!isMobile && (
+        <div className="bg-light-cream text-light-black h-screen max-md:h-100 text-8xl font-lt">
+          <ImageParallax />
+        </div>
+      )}
       <div className="bg-light-black text-light-cream min-h-screen">
         <Work />
       </div>
